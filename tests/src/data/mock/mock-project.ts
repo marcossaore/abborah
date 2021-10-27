7957c5937222d6367f7332b79f85c1ec06354144
Marcos Soares <marcossaore@gmail.com>
2021-10-27 20:37:26 -0300

    test: ensure DbAddProject calls AddProjectRepository with correct values

---
+++ tests/src/data/mock/mock-project.ts
@@ -0,0 +1,28 @@
+import { AddProjectRepository } from '@/data/protocols/project/db-add-project-repository'
+import { ProjectModel } from '@/domain/models/project/project'
+import { AddProjectParams } from '@/domain/usecases/project/add-project'
+
+export const mockProject = (): AddProjectParams => ({
+  name: 'Project name',
+  description: 'Project description',
+  startDate: new Date(),
+  endDate: new Date()
+})
+
+export const mockProjectModel = (): ProjectModel => ({
+  id: 1,
+  name: 'Project name',
+  description: 'Project description',
+  startDate: new Date(),
+  endDate: new Date()
+})
+
+export class AddProjectRepositorySpy implements AddProjectRepository {
+  projectParams: AddProjectParams
+  projectResult: ProjectModel
+  async add (project: AddProjectRepository.Params): Promise<AddProjectRepository.Result> {
+    this.projectParams = project
+    this.projectResult = mockProjectModel()
+    return Promise.resolve(this.projectResult)
+  }
+}
---
+++ tests/src/data/usecases/project/add-project.spec.ts
@@ -0,0 +1,39 @@
+import { DbAddProject } from '@/data/usecases/project/db-add-project'
+import { AddProjectRepositorySpy, mockProject } from '../../mock'
+import MockDate from 'mockdate'
+
+type SutTypes = {
+  sut: DbAddProject
+  addProjectRepository: AddProjectRepositorySpy
+}
+
+const makeSut = (): SutTypes => {
+  const addProjectRepository = new AddProjectRepositorySpy()
+  const sut = new DbAddProject(addProjectRepository)
+  return {
+    sut,
+    addProjectRepository
+  }
+}
+
+describe('DbAddProject UseCase', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
+  it('should call AddProjectRepository with correct values', async () => {
+    const { sut, addProjectRepository } = makeSut()
+    const mock = mockProject()
+    await sut.add(mock)
+    expect(addProjectRepository.projectParams).toEqual(mock)
+    expect(addProjectRepository.projectResult.id).toBeTruthy()
+    expect(addProjectRepository.projectResult.name).toBe(mock.name)
+    expect(addProjectRepository.projectResult.description).toBe(mock.description)
+    expect(addProjectRepository.projectResult.startDate).toEqual(mock.startDate)
+    expect(addProjectRepository.projectResult.endDate).toEqual(mock.endDate)
+  })
+})
