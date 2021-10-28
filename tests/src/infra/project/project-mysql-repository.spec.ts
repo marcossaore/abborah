bb95b47fd90ecf8944a25ab38e2aa005b8746025
Marcos Soares <marcossaore@gmail.com>
2021-10-28 00:35:28 -0300

    test: ensure ProjectMySqlRepository saves project on success

---
+++ tests/src/infra/project/project-mysql-repository.spec.ts
@@ -0,0 +1,32 @@
+import { ProjectMysqlRepository } from '@/infra/project/project-mysql-repository'
+import prisma from '@/infra/prisma-adapter/client'
+import { mockProject } from '../../data/mock'
+
+const makeSut = (): ProjectMysqlRepository => {
+  return new ProjectMysqlRepository()
+}
+
+describe('ProjectMysqlRepository', () => {
+  afterAll(async () => {
+    await prisma.$connect()
+  })
+
+  beforeEach(async () => {
+    await prisma.project.deleteMany()
+  })
+
+  beforeAll(async () => {
+    await prisma.$disconnect()
+  })
+
+  test('should save a new project in db', async () => {
+    const sut = makeSut()
+    const mock = mockProject()
+    const project = await sut.add(mock)
+    expect(project.id).toBeTruthy()
+    expect(project.name).toBe(mock.name)
+    expect(project.description).toBe(mock.description)
+    expect(project.startDate).toEqual(mock.startDate)
+    expect(project.endDate).toEqual(mock.endDate)
+  })
+})
