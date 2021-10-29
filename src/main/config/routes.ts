6d1da7ca0ef96bec7142a574da9acf645198256a
Marcos Soares <marcossaore@gmail.com>
2021-10-29 19:35:39 -0300

    fix: add helper for path to mount dynamic route correctly

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,11 +1,14 @@
 import { Express, Router } from 'express'
 import { readdirSync } from 'fs'
+import { join } from 'path'
 
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
   // eslint-disable-next-line node/no-path-concat
-  readdirSync(`${__dirname}/../routes`).map(async file => {
-    (await import(`../routes/${file}`)).default(router)
+  readdirSync(join(__dirname, '../routes')).map(async file => {
+    if (!file.endsWith('.map')) {
+      (await import(`../routes/${file}`)).default(router)
+    }
   })
 }
