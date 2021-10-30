d0fa9b7be0a58bdd32b3307fb4e29a246faf7358
Marcos Soares <marcossaore@gmail.com>
2021-10-30 18:13:26 -0300

    fix: change path file reference to validation project protocols

--- src/main/factories/controllers/project/add-project-validation-factory.ts
+++ src/main/factories/controllers/project/add-project-validation-factory.ts
@@ -1,6 +1,5 @@
-import { ValidationFactory } from '../../../../validation/validation-factory'
-import { AddProjectValidationModel } from '@/validation/validators/models/models'
-
+import { AddProjectValidationModel } from '@/validation/project/add-project'
+import { ValidationFactory } from '@/validation/validation-factory'
 export class AddProjectValidationFactory extends ValidationFactory {
   constructor () {
     super(AddProjectValidationModel)
