693ca1ec22edec1676149e044674b1f55f7b8243
Marcos Soares <marcossaore@gmail.com>
2021-10-30 20:33:07 -0300

    test: ensure ProjectController returns startDate with valid date when it is not provided

--- tests/src/main/routes/project-routes.test.ts
+++ tests/src/main/routes/project-routes.test.ts
@@ -1,7 +1,7 @@
 import app from '@/main/config/app'
 import prisma from '@/infra/prisma-adapter/client'
-import { mockProject } from '../../data/mock'
 import request from 'supertest'
+import { mockRequest } from '../../presentation/mock'
 
 describe('Project Routes', () => {
   beforeEach(async () => {
@@ -14,11 +14,27 @@ describe('Project Routes', () => {
 
   describe('POST /projects', () => {
     test('Should return 200 on success', async () => {
-      const project = mockProject()
+      const project = mockRequest()
       await request(app)
         .post('/api/projects')
         .send(project)
         .expect(200)
+    })
+
+    test('Should return 200 and a valid date when startDate is not provided', async () => {
+      const project = mockRequest()
+      project.startDate = undefined
+      await request(app)
+        .post('/api/projects')
+        .send(project)
+        .expect(200)
+        .then(response => {
+          const today = new Date()
+          const createdDate = new Date(response.body.startDate)
+          expect(createdDate.getDate()).toBe(today.getDate())
+          expect(createdDate.getMonth()).toBe(today.getMonth())
+          expect(createdDate.getFullYear()).toBe(today.getFullYear())
+        })
     })
   })
 })
