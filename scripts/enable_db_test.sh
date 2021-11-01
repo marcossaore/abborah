8324d6d07e03dac6ee576b7ec1f0e510613a1943
Marcos Soares <marcossaore@gmail.com>
2021-11-01 19:58:24 -0300

    feat: add enviroment docker-compose to running tests on Actions #3

--- scripts/enable_db_test.sh
+++ scripts/enable_db_test.sh
@@ -2,7 +2,7 @@
 HAS_DOCKER_COMPOSE=$($(docker-compose -v) && echo "YES" || echo "NO");
 
 if [ "$HAS_DOCKER_COMPOSE" == "YES" ]; then
-    docker-compose -f docker-compose.test.yml exec db_integration pwd || docker-compose up -d db_integration
+  docker-compose -f docker-compose.test.yml up -d db_integration
 fi
 
 yarn prisma generate && \
