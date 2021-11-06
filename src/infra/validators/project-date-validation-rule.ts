f123e18ac69cca7830b6048802f8f0eb8560d499
Marcos Soares <marcossaore@gmail.com>
2021-11-06 14:29:36 -0300

    chore: improve ProjectDateValidationRule code

--- src/infra/validators/project-date-validation-rule.ts
+++ src/infra/validators/project-date-validation-rule.ts
@@ -3,14 +3,17 @@ import { InvalidStartProjectDateError, ProjectInvalidDateRangeError } from '@/pr
 
 export class ProjectDateValidationRule implements ValidationRule {
   validate (input: ProjectDateRule.Params): Error {
+    const dayTime = (1000 * 3600 * 24)
+
     const today = new Date()
 
     const startDate = input.startDate ? new Date(input.startDate) : new Date(today)
+
     const endDate = new Date(input.endDate)
 
     const differenceBetweenEndDateAndStartDateInTime = endDate.getTime() - startDate.getTime()
 
-    const differenceBetweenEndDateAndStartdateInDays = differenceBetweenEndDateAndStartDateInTime / (1000 * 3600 * 24)
+    const differenceBetweenEndDateAndStartdateInDays = differenceBetweenEndDateAndStartDateInTime / dayTime
 
     if (differenceBetweenEndDateAndStartdateInDays >= 30) {
       return new InvalidStartProjectDateError()
