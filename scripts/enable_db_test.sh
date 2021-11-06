497fff7be8b501407e49a455c347a701a13b3883
Marcos Soares <marcossaore@gmail.com>
2021-11-06 13:33:17 -0300

    chore: improve script that runs before tests

--- scripts/enable_db_test.sh
+++ scripts/enable_db_test.sh
@@ -5,15 +5,17 @@ if [ "$HAS_DOCKER_COMPOSE" == "YES" ]; then
 
   if [ -z `docker-compose -f docker-compose.test.yml ps --services --filter "status=running" | grep db_integration` ]; then
     docker-compose -f docker-compose.test.yml up -d db_integration
-    yarn prisma generate 
   fi 
 fi
 
-  while true; do 
+  yarn prisma migrate status 
+
+  while $! -eq 0; do
+    yarn prisma generate  
     yarn prisma migrate dev
     if [[ "$?" -ne 1 ]]; then 
       break
     fi
     echo "Waiting for db to be ready..."
-    sleep 20
+    sleep 10
   done
