fc3191ccb216f1cf74e3843074b6985db1f5241c
Marcos Soares <marcossaore@gmail.com>
2021-11-06 13:21:07 -0300

    test: ensure DbLoadProjectById throws if LoadProjectByIdRepository throws

--- tests/src/data/usecases/project/db-load-project-by-id.spec.ts
+++ tests/src/data/usecases/project/db-load-project-by-id.spec.ts
@@ -1,4 +1,5 @@
 import { DbLoadProjectById } from '@/data/usecases/project/db-load-project-by-id'
+import { mockThrowError } from '@/tests/src/mock-utils'
 import { LoadProjectByIdRepositorySpy } from '../../mock/mock-task'
 
 type SutTypes = {
@@ -17,10 +18,17 @@ const makeSut = (): SutTypes => {
 
 const mockId = 3
 
-describe('DbAddProject UseCase', () => {
+describe('DbLoadProjectById UseCase', () => {
   test('should call LoadProjectByIdRepository with correct id', async () => {
     const { sut, loadProjectByIdRepositorySpy } = makeSut()
     await sut.load(mockId)
     expect(loadProjectByIdRepositorySpy.id).toEqual(mockId)
+  })
+
+  test('should throws if LoadProjectByIdRepository throws', async () => {
+    const { sut, loadProjectByIdRepositorySpy } = makeSut()
+    jest.spyOn(loadProjectByIdRepositorySpy, 'load').mockImplementationOnce(mockThrowError)
+    const promise = sut.load(mockId)
+    await expect(promise).rejects.toThrow()
   })
 })
