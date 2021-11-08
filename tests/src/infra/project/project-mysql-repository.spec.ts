f32e3e1a573cf194a4eebca0854972cf92399a28
Marcos Soares <marcossaore@gmail.com>
2021-11-07 21:44:04 -0300

    test: ensure ProjectMysqlRepository.loadById returns correct project

--- tests/src/infra/project/project-mysql-repository.spec.ts
+++ tests/src/infra/project/project-mysql-repository.spec.ts
@@ -19,14 +19,31 @@ describe('ProjectMysqlRepository', () => {
     await prisma.$disconnect()
   })
 
-  test('should save a new project in db', async () => {
-    const sut = makeSut()
-    const mock = mockProject()
-    const project = await sut.add(mock)
-    expect(project.id).toBeTruthy()
-    expect(project.name).toBe(mock.name)
-    expect(project.description).toBe(mock.description)
-    expect(project.startDate).toEqual(mock.startDate)
-    expect(project.endDate).toEqual(mock.endDate)
+  describe('#add()', () => {
+    test('should save a new project in db', async () => {
+      const sut = makeSut()
+      const mock = mockProject()
+      const project = await sut.add(mock)
+      expect(project.id).toBeTruthy()
+      expect(project.name).toBe(mock.name)
+      expect(project.description).toBe(mock.description)
+      expect(project.startDate).toEqual(mock.startDate)
+      expect(project.endDate).toEqual(mock.endDate)
+    })
+  })
+
+  describe('#loadById()', () => {
+    test('should load a project', async () => {
+      const projectSaved = await prisma.project.create({
+        data: mockProject()
+      })
+      const sut = makeSut()
+      const projectLoaded = await sut.loadById(projectSaved.id)
+      expect(projectLoaded.id).toBe(projectSaved.id)
+      expect(projectLoaded.name).toBe(projectSaved.name)
+      expect(projectLoaded.description).toBe(projectSaved.description)
+      expect(projectLoaded.startDate).toEqual(projectSaved.startDate)
+      expect(projectLoaded.endDate).toEqual(projectSaved.endDate)
+    })
   })
 })
