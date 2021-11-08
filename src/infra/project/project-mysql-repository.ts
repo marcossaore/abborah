5dff7ad7dbe27fb5f45004dd351fb8bfc4015ed3
Marcos Soares <marcossaore@gmail.com>
2021-11-07 21:43:26 -0300

    feat: ensure ProjectMysqlRepository.loadById returns correct project

--- src/infra/project/project-mysql-repository.ts
+++ src/infra/project/project-mysql-repository.ts
@@ -2,11 +2,20 @@ import prisma from '@/infra/prisma-adapter/client'
 import { AddProjectRepository } from '@/data/protocols/project/add-project-repository'
 import { ProjectModel } from '@/domain/models/project/project'
 import { AddProjectParams } from '@/domain/usecases/project/add-project'
+import { LoadProjectByIdRepository } from '@/data/protocols/project/load-project-by-id-repository'
 
-export class ProjectMysqlRepository implements AddProjectRepository {
+export class ProjectMysqlRepository implements AddProjectRepository, LoadProjectByIdRepository {
   async add (projectParam: AddProjectParams): Promise<ProjectModel> {
     return await prisma.project.create({
       data: projectParam
+    })
+  }
+
+  async loadById (id: number): Promise<ProjectModel> {
+    return await prisma.project.findFirst({
+      where: {
+        id
+      }
     })
   }
 }
